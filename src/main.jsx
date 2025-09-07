86d6d5dc61c800ebb26d76dc68e62df5ba842e7f
Adisdzaky <adisdzakyrivai@gmail.com>
2025-09-08 05:16:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ createRoot(document.getElementById('root')).render(
       <App />
   </StrictMode>
 )
-
 // register service worker + notifikasi update
 const updateSW = registerSW({
   onNeedRefresh() {
