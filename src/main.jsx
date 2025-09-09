8ea4a90c9b82b09311be840306f158c4ed2e9fa1
Adisdzaky <adisdzakyrivai@gmail.com>
2025-09-09 16:47:28 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import './index.css'
 import App from './App.jsx'
-import { registerSW } from "virtual:pwa-register";
+//import { registerSW } from "virtual:pwa-register";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
